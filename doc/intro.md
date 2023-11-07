168b767d0e344be1b3ce8e18115e90458e53723f
lotuc <lotu.c@outlook.com>
2023-11-07 22:35:49 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to backoff
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lotuc/backoff_test.clj
@@ -0,0 +1,7 @@
+(ns lotuc.backoff-test
+  (:require [clojure.test :refer :all]
+            [lotuc.backoff :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
