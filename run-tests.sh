c6012f36ddd2e5b921f398f5bf3da6917e57fe12
lotuc <lotu.c@outlook.com>
2023-11-13 19:24:00 +0800

    chore: tests over Clojure 1.10 & 1.11

---
+++ run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# core.async declared its Clojure dependencies to be 1.10, so here we only test
+# over 1.10 and higher versions of Clojure
+#
+# https://github.com/clojure/core.async/blob/master/project.clj
+
+versions="1.10 1.11"
+for v in $versions
+do
+  clojure -M:test:"$v"
+done
